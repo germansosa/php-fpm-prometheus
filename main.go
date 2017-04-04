b2c6ef22cf802b187e25fe509dc58524a7de83e1
germansosa <gskgoskk@gmail.com>
2017-04-04 01:09:53 -0300

    + Connect to fpm directly over fastcgi

--- main.go
+++ main.go
@@ -2,63 +2,81 @@ package main
 
 import (
 	"flag"
+	"github.com/tomasen/fcgi_client"
 	"gopkg.in/tylerb/graceful.v1"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
-	"time"
 	"strconv"
+	"time"
 )
 
 var (
 	statusLineRegexp = regexp.MustCompile(`(?m)^(.*):\s+(.*)$`)
-	fpmStatusURL     = ""
+	fpmStatusHost    = ""
+	fpmStatusPort    = ""
 )
 
 func main() {
-	url := flag.String("status-url", "", "PHP-FPM status URL")
+	urlhost := flag.String("status-host", "", "PHP-FPM status host")
+	urlport := flag.String("status-port", "9000", "PHP-FPM status port")
 	addr := flag.String("addr", "0.0.0.0:8080", "IP/port for the HTTP server")
 	flag.Parse()
 
-	if *url == "" {
-		log.Fatal("The status-url flag is required.")
+	if *urlhost == "" {
+		log.Fatal("The status-host flags is required.")
 	} else {
-		fpmStatusURL = *url
+		fpmStatusHost = *urlhost
 	}
+	fpmStatusPort = *urlport
 
 	scrapeFailures := 0
 
+	// TODO: Migrate to go 1.8 with built in graceful shutdown
 	server := &graceful.Server{
 		Timeout: 10 * time.Second,
 		Server: &http.Server{
 			Addr:        *addr,
 			ReadTimeout: time.Duration(5) * time.Second,
 			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				resp, err := http.Get(fpmStatusURL)
+				env := make(map[string]string)
+				env["SCRIPT_FILENAME"] = "fpm-status"
+
+				fcgi, err := fcgiclient.Dial(fpmStatusHost, fpmStatusPort)
 
 				if err != nil {
 					log.Println(err)
-					scrapeFailures = scrapeFailures+1
+					scrapeFailures = scrapeFailures + 1
 					x := strconv.Itoa(scrapeFailures)
-					NewMetricsFromMatches([][]string{{"scrape failure:","scrape failure",x}}).WriteTo(w)
+					NewMetricsFromMatches([][]string{{"scrape failure:", "scrape failure", x}}).WriteTo(w)
 					return
 				}
 
-				if (resp.StatusCode != http.StatusOK){
-					log.Println("php-fpm status code is not OK.")
-					scrapeFailures = scrapeFailures+1
+				resp, err := fcgi.Get(env)
+
+				if err != nil {
+					log.Println(err)
+					scrapeFailures = scrapeFailures + 1
 					x := strconv.Itoa(scrapeFailures)
-					NewMetricsFromMatches([][]string{{"scrape failure:","scrape failure",x}}).WriteTo(w)
+					NewMetricsFromMatches([][]string{{"scrape failure:", "scrape failure", x}}).WriteTo(w)
+					return
+				}
+
+				if resp.StatusCode != http.StatusOK {
+					log.Println("php-fpm status code is not OK.")
+					scrapeFailures = scrapeFailures + 1
+					x := strconv.Itoa(scrapeFailures)
+					NewMetricsFromMatches([][]string{{"scrape failure:", "scrape failure", x}}).WriteTo(w)
 					return
 				}
 
 				body, err := ioutil.ReadAll(resp.Body)
 				if err != nil {
 					log.Println(err)
-					scrapeFailures = scrapeFailures+1
+					scrapeFailures = scrapeFailures + 1
 					x := strconv.Itoa(scrapeFailures)
-					NewMetricsFromMatches([][]string{{"scrape failure:","scrape failure",x}}).WriteTo(w)
+					NewMetricsFromMatches([][]string{{"scrape failure:", "scrape failure", x}}).WriteTo(w)
 					return
 				}
 
@@ -67,7 +85,7 @@ func main() {
 				x := strconv.Itoa(scrapeFailures)
 
 				matches := statusLineRegexp.FindAllStringSubmatch(string(body), -1)
-				matches = append(matches,[]string{"scrape failure:","scrape failure",x})
+				matches = append(matches, []string{"scrape failure:", "scrape failure", x})
 
 				NewMetricsFromMatches(matches).WriteTo(w)
 			}),
